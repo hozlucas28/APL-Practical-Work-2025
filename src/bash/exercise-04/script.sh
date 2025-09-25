eb146dc4cf811087d0010c27a41dbd91bfad5b72
hozlucas28 <hozlucas28@gmail.com>
2025-09-25 15:36:48 -0300

    fix(bash): exercise-04
    Remove unnecessary conditional on regex and literal string.

--- src/bash/exercise-04/script.sh
+++ src/bash/exercise-04/script.sh
@@ -77,25 +77,14 @@ iniciar_demonio() {
                     patron="$(printf '%s' "$raw_patron" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
                     [[ -z "$patron" ]] && continue
 
-                    if [[ $patron == regex:* ]]; then
-                        # Búsqueda regex
-                        pattern="${patron#regex:}" # Eliminar prefijo "regex:"
+                    # Eliminar el prefijo 'regex:'
+                    patron="${patron#regex:}"
 
-                        if grep -En "$pattern" "$REPO/$archivo" >/dev/null 2>&1; then
-                            mensaje="Alerta: patrón '$patron' encontrado en el archivo '$(realpath "$archivo")'."
+                    if grep -E "$patron" "$REPO/$archivo" >/dev/null 2>&1; then
+                        mensaje="Alerta: patrón '$patron' encontrado en el archivo '$(realpath "$archivo")'."
 
-                            if ! grep -F -q "$mensaje" "$LOG" 2>/dev/null; then
-                                echo "[$(date '+%Y-%m-%d %H:%M:%S')] $mensaje" | tee -a "$LOG" >/dev/null
-                            fi
-                        fi
-                    else
-                        # Búsqueda literal
-                        if grep -F -n "$patron" "$REPO/$archivo" >/dev/null 2>&1; then
-                            mensaje="Alerta: patrón '$patron' encontrado en el archivo '$(realpath "$archivo")'."
-
-                            if ! grep -F -q "$mensaje" "$LOG" 2>/dev/null; then
-                                echo "[$(date '+%Y-%m-%d %H:%M:%S')] $mensaje" | tee -a "$LOG" >/dev/null
-                            fi
+                        if ! grep -F -q "$mensaje" "$LOG" 2>/dev/null; then
+                            echo "[$(date '+%Y-%m-%d %H:%M:%S')] $mensaje" | tee -a "$LOG" >/dev/null
                         fi
                     fi
                 done < "$CONFIG"
